7ba8f61de05bbfc2608d3920771d2da358747176
abhidharshss <abhidharsh1999@gmail.com>
2024-02-02 11:48:09 +0530

    final commit

--- API/models.py
+++ API/models.py
@@ -6,12 +6,13 @@ from API import permissions
 
 # Create your models here.
 
+
 class users(AbstractUser):
-    email=models.CharField(max_length=80,unique=True)
-    username=models.CharField(max_length=45)
-    is_client=models.BooleanField(default=False)
-    is_verified=models.BooleanField(default=False)
-    otp=models.CharField(max_length=6,null=True,blank=True)
+    email = models.CharField(max_length=80, unique=True)
+    username = models.CharField(max_length=45)
+    is_client = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
+    otp = models.CharField(max_length=6, null=True, blank=True)
     otp_expiration = models.DateTimeField(null=True, blank=True)
 
     # Define unique related_name attributes to resolve the clash
@@ -41,9 +42,10 @@ class users(AbstractUser):
     def __str__(self):
         return self.email
 
+
 class Files(models.Model):
-    file=models.FileField(upload_to='Uploads')
-    Uploaded_at=models.DateField(auto_now_add=True)
+    file = models.FileField(upload_to='Uploads')
+    Uploaded_at = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.file.name
